4808713459acd95eda439331ed805d72d78480e1
Tim Mecklem <timothy@mecklem.com>
2017-11-18 21:45:17 -0500

    Update dependencies for pump chip autodetect support

--- fw/config/infinity_rpi0.exs
+++ fw/config/infinity_rpi0.exs
@@ -10,7 +10,13 @@ config :infinity_aps,
   host_mode: false
 
 config :pummpcomm, :pump, Pummpcomm.Session.Pump
-config :pummpcomm, :cgm, Pummpcomm.Session.Pump
+config :pummpcomm, :cgm, Pummpcomm.Session.PumpFake
+
+config :pummpcomm, :autodetect_chips, [
+  %{__struct__: RFM69.Device, name: :ecc1_phat, device: "spidev0.0", reset_pin: 24, interrupt_pin: 23},
+  %{__struct__: SubgRfspy.SPI, name: :explorer_board, device: "spidev0.0", reset_pin: 4},
+  %{__struct__: SubgRfspy.UART, name: :slice_of_radio, device: "/dev/ttyAMA0"},
+]
 
 config :pummpcomm, :autodetect_chips, [
   %{__struct__: RFM69.Device, name: :ecc1_phat, device: "spidev0.0", reset_pin: 24, interrupt_pin: 23},
