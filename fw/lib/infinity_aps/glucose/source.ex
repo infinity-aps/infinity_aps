dff7197f7b348929fef1d25b1070722088230b53
Tim Mecklem <timothy@mecklem.com>
2017-12-24 15:16:28 -0500

    Add TwilightInformant as Glucose source

--- fw/lib/infinity_aps/glucose/source.ex
+++ fw/lib/infinity_aps/glucose/source.ex
@@ -5,3 +5,22 @@ end
 defimpl InfinityAPS.Glucose.Source, for: Pummpcomm.Monitor.BloodGlucoseMonitor do
   defdelegate get_sensor_values(source, minutes_back, timezone), to: Pummpcomm.Monitor.BloodGlucoseMonitor
 end
+
+defmodule InfinityAPS.TwilightInformant do
+  defstruct [:ns_url, :api_secret, :httpoison_opts]
+end
+
+defimpl InfinityAPS.Glucose.Source, for: InfinityAPS.TwilightInformant do
+  require Logger
+  def get_sensor_values(_source, minutes_back, timezone) do
+    time_back = minimum_date(minutes_back, timezone)
+    result = TwilightInformant.entries(count: 600, "find[dateString][$gte]": time_back)
+    Logger.error "Result: #{inspect result}"
+    result
+  end
+
+  def minimum_date(minutes_back, timezone) do
+    # :utc |> Timex.now() |>  Timex.shift(minutes: -minutes_back) |> Timex.format!("{ISO:Extended:Z}")
+    timezone |> Timex.now() |> Timex.shift(minutes: -minutes_back) |> DateTime.to_naive
+  end
+end
