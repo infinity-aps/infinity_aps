85357cf96a6f3bc5b574b2845c62e3a53008f537
Tim Mecklem <timothy@mecklem.com>
2018-02-23 10:55:17 -0600

    Remove unused code

--- aps/test/aps_test.exs
+++ aps/test/aps_test.exs
@@ -1,8 +1,4 @@
 defmodule InfinityAPSTest do
   use ExUnit.Case
   doctest InfinityAPS
-
-  test "greets the world" do
-    assert InfinityAPS.hello() == :world
-  end
 end
--- ui/lib/ui_web/channels/loop_status_channel.ex
+++ ui/lib/ui_web/channels/loop_status_channel.ex
@@ -3,20 +3,10 @@ defmodule InfinityAPS.UI.LoopStatusChannel do
 
   require Logger
 
-  alias InfinityAPS.Configuration.Server
-  alias InfinityAPS.Glucose.Source
   alias InfinityAPS.UI.GlucoseBroker
-  alias Pummpcomm.Monitor.BloodGlucoseMonitor
-  alias Pummpcomm.Monitor.HistoryMonitor
 
-  @minutes_back 720
   def join("loop_status:glucose", _message, socket) do
     send(self(), {:after_join_glucose})
-    {:ok, socket}
-  end
-
-  def join("loop_status:basal", _message, socket) do
-    send(self(), {:after_join_basal})
     {:ok, socket}
   end
 
@@ -32,36 +22,5 @@ defmodule InfinityAPS.UI.LoopStatusChannel do
     end
 
     {:noreply, socket}
-  end
-
-  def handle_info({:after_join_basal}, socket) do
-    case HistoryMonitor.get_pump_history(@minutes_back, local_timezone()) do
-      {:ok, history} ->
-        # message = map_history(history, local_timezone())
-        message = history
-        push socket, "basal", %{data: message}
-      response       ->
-        Logger.warn "Got: #{inspect(response)}"
-    end
-    {:noreply, socket}
-  end
-
-  defp map_entries(entries, local_timezone) do
-    entries
-    |> Enum.filter(&filter_sgv/1)
-    |> Enum.map(fn(entry) -> map_sgv(entry, local_timezone) end)
-  end
-
-  defp filter_sgv({:sensor_glucose_value, _}), do: true
-  defp filter_sgv(_),                          do: false
-
-  defp map_sgv({:sensor_glucose_value, entry_data}, local_timezone) do
-    date_with_zone = Timex.to_datetime(entry_data.timestamp, local_timezone)
-    dateString = Timex.format!(date_with_zone, "{ISO:Extended:Z}")
-    %{type: "sgv", sgv: entry_data.sgv, dateString: dateString}
-  end
-
-  defp local_timezone do
-    Server.get_config(:timezone) |> Timex.Timezone.get()
   end
 end
