399c234f264abd05bcf4874d5d1e96c901eb2a7e
David Gil <davidgiloliva@gmail.com>
2018-05-15 13:10:38 +0200

    Fix losing log messages

--- ui/lib/ui_web/channels/log_channel.ex
+++ ui/lib/ui_web/channels/log_channel.ex
@@ -6,7 +6,13 @@ defmodule InfinityAPS.UI.LogChannel do
   alias InfinityAPS.UI.Client
 
   def join("logs", _message, socket) do
-    Client.attach()
+    send(self(), {:after_join_attach})
     {:ok, socket}
+  end
+
+  def handle_info({:after_join_attach}, socket) do
+    Client.tail()
+    Client.attach()
+    {:noreply, socket}
   end
 end
